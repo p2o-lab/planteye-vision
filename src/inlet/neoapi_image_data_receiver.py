f78b8aedc077e8cd0cc1279337a85b23f402aa2c
Valentin Khaydarov <valentin.khaydarov@gmail.com>
2022-01-17 14:58:46 +0100

    Baumer camera as inlet added

--- src/inlet/neoapi_image_data_receiver.py
+++ src/inlet/neoapi_image_data_receiver.py
@@ -3,29 +3,34 @@ import numpy as np
 import neoapi
 from time import time, sleep
 
-from src.receive.camera_image_data_receiver import CameraImageDataReceiver
+import logging
+from time import sleep
+import cv2
+
+from src.inlet.camera_inlet import CameraInlet
+from src.common.timestamp import get_timestamp
+from src.data_chunks.data_chunk import GeneralDataChunk
+from src.data_chunks.data_chunk_status import CapturingStatus
+from src.data_chunks.metadata_chunk import MetadataChunkData
+from src.data_chunks.data_chunk_data import DataChunkImage
+from src.configuration.inlet_configuration import CameraConfiguration
 
 GET_IMAGE_TIMEOUT = 1000
 
 
-class NeoApiImageDataReceiver(CameraImageDataReceiver):
-    def __init__(self, cfg: dict):
-        super().__init__()
+class NeoApiImageDataReceiver(CameraInlet):
 
-    def import_config(self, cfg_provider):
-        """
-        Takes parameter set from configuration and apply them to capturing device
-        :return:
-        """
-        super().import_config(cfg_provider)
+    def __init__(self, config: CameraConfiguration):
+        super().__init__(config)
+        self.camera_status.initialised = False
 
-    def configure(self):
-        """
-        Takes parameter set from configuration and apply them to capturing device
-        :return:
-        """
-        super().configure()
-        self.camera.SetSynchronFeatureMode(True)
+    def __del__(self):
+        if self.camera_object is not None:
+            self.disconnect()
+
+    def apply_configuration(self):
+        self.camera_object.SetSynchronFeatureMode(True)
+        super().apply_configuration()
 
     def set_parameter(self, feature: str, requested_value) -> bool:
         """
@@ -37,44 +42,117 @@ class NeoApiImageDataReceiver(CameraImageDataReceiver):
         :return: bool: True if the feature is set successfully, False - if not
         """
 
-        if not self.camera.HasFeature(feature):
+        if not self.camera_object.HasFeature(feature):
             logging.warning('Feature %s not detected' % feature)
             return False
         else:
             logging.info('Feature %s detected' % feature)
 
-        if not self.camera.IsWritable(feature):
+        if not self.camera_object.IsWritable(feature):
             logging.warning('Feature %s not available' % feature)
             return False
         else:
             logging.info('Feature %s writable' % feature)
 
         try:
-            self.camera.SetFeature(feature, requested_value)
-            new_value = str(self.camera.GetFeature(feature))
+            self.camera_object.SetFeature(feature, requested_value)
+            new_value = str(self.camera_object.GetFeature(feature))
             logging.info('Feature %s set to %s' % (feature, new_value))
             return True
         except neoapi.FeatureAccessException as exc:
             logging.warning('Feature %s cannot be set to %s due to %s' % (feature, requested_value, exc))
             return False
 
-    def initialise(self):
-        """
-        Initialises capturing device.
-        :return:
-        """
-        self.camera = neoapi.Cam()
-
-        while not self.camera.IsConnected():
-            self.connect()
+    def connect(self):
+        self.camera_object = neoapi.Cam()
+        self.camera_status.initialised = False
+        while not self.camera_object.IsConnected():
+            self._connect_attempt()
             sleep(1)
 
-        self.initialised = True
+        self.camera_status.initialised = True
         logging.info('Capturing device initialised successfully')
         logging.info(self.get_details())
 
+    def _connect_attempt(self):
+        logging.info('Trying to connect to capturing device...')
+        try:
+            self.camera_object.Connect(self.config.parameters['device_id'])
+        except neoapi.NotConnectedException as exc:
+            logging.error('Capturing device not connected... trying again', exc_info=exc)
+        except neoapi.NoAccessException as exc:
+            logging.error('Capturing device not connected... trying again', exc_info=exc)
+
+    def disconnect(self):
+        if not self.camera_status.initialised:
+            logging.warning('No device initialised, nothing to release')
+            return
+
+        self.camera_object = None
+        logging.info('Captured device released')
+        self.camera_status.initialised = True
+
+    def retrieve_data(self):
+        data_chunk = GeneralDataChunk(self.name, self.type, self.config.parameters, hidden=self.config.hidden)
+
+        if not self.config.is_valid():
+            status = CapturingStatus(100)
+            data_chunk.add_status(status)
+            print('Step %s : No execution due to invalid configuration' % self.name)
+            return [data_chunk]
+
+        if not self.camera_status.initialised:
+            self.connect()
+
+        timestamp = MetadataChunkData('timestamp', get_timestamp())
+        data_chunk.add_metadata(timestamp)
+
+        if not self.camera_status.initialised:
+            status = CapturingStatus(1)
+            data_chunk.add_status(status)
+            logging.warning(status.get_message())
+            self.camera_status.initialised = False
+            return [data_chunk]
+
+        if self.camera_status.capturing:
+            status = CapturingStatus(2)
+            data_chunk.add_status(status)
+            logging.warning(status.get_message())
+            self.camera_status.initialised = False
+            return [data_chunk]
+
+        self.camera_status.capturing = True
+        frame_raw = self.camera_object.GetImage(GET_IMAGE_TIMEOUT)
+        self.camera_status.capturing = False
+
+        frame_shape = frame_raw.shape
+        frame_colormap = 'BGR'
+        frame_np = frame_raw.GetNPArray()
+
+        if frame_raw is not None:
+            data_chunk.add_data(DataChunkImage('frame', frame_np, 'base64_png'))
+
+            status = CapturingStatus(0)
+            data_chunk.add_status(status)
+            logging.warning(status.get_message())
+
+            data_chunk.add_metadata(MetadataChunkData('colormap', 'BGR'))
+            data_chunk.add_metadata(MetadataChunkData('shape', frame_np.shape))
+            for metadata_variable, metadata_value in self.config.metadata.items():
+                data_chunk.add_metadata(MetadataChunkData(metadata_variable, metadata_value))
+            return [data_chunk]
+        else:
+            status = CapturingStatus(99)
+            data_chunk.add_status(status)
+            logging.warning(status.get_message())
+            self.camera_status.initialised = False
+            return [data_chunk]
+
+    def execute(self):
+        return super().execute()
+
     def get_details(self):
-        if not self.camera.IsConnected():
+        if not self.camera_object.IsConnected():
             return {}
         return {
             'id': neoapi.CamInfo().GetId(),
@@ -84,72 +162,6 @@ class NeoApiImageDataReceiver(CameraImageDataReceiver):
         }
 
     def get_configuration(self):
-        if not self.camera.IsConnected():
+        if not self.camera_object.IsConnected():
             return {}
         return {f.GetName(): f.GetValue() for f in neoapi.FeatureList}
-
-    def connect(self):
-        try:
-            self.camera.Connect(self.cfg['connection']['device_id'])
-        except neoapi.NotConnectedException as exc:
-            logging.error('Capturing device not connected... trying again', exc_info=exc)
-        except neoapi.NoAccessException as exc:
-            logging.error('Capturing device not connected... trying again', exc_info=exc)
-
-    def receive_data(self) -> (bool, np.array, int):
-        """
-        Captures single frame and returns it as a numpy array
-        :return: Tuple of three variables:
-        status: dict: Status in a json format as a python dict
-        frame: np.array: Frame in the form of np.array
-        timestamp: int: Unix timestamp in milliseconds
-        """
-        if self.capturing:
-            status = {'code': 500, 'message': 'Capturing device busy'}
-            return status, None, int(round(time() * 1000))
-
-        if not self.camera.IsConnected():
-            self.connect()
-
-        if not self.camera.IsConnected():
-            status = {'code': 500, 'message': 'Connection to capturing device cannot be established'}
-            return status, None, int(round(time() * 1000))
-
-        self.capturing = True
-        frame_raw = self.camera.GetImage(GET_IMAGE_TIMEOUT)
-        timestamp = int(round(time() * 1000))
-        self.capturing = False
-
-        frame_shape = frame_raw.shape
-        frame_colormap = 'BGR'
-        frame = {'frame': frame_raw.GetNPArray(), 'frame_shape': frame_shape, 'frame_colormap': frame_colormap}
-
-        if frame_raw.IsEmpty():
-            status = {'code': 500, 'message': 'Error capturing frame'}
-            return status, None, timestamp
-
-        logging.info('Frame captured')
-        status = {'code': 200, 'message': 'Frame captured'}
-        return status, frame, timestamp
-
-    def get_status(self) -> dict:
-        """
-        Gets status of capturing device including whether the device initialised or not, is currently capturing or idle
-        along with unix timestamp in milliseconds.
-        :return: dict: Status in form of {'status: {'connected': Value, 'capturing': Value}, 'timestamp': Value}
-        """
-        if self.camera.IsConnected():
-            msg = 'camera connected'
-        else:
-            msg = 'camera not connected'
-        connection_status = {'status': self.camera.IsConnected(), 'message': msg}
-
-        if self.capturing:
-            msg = 'camera capturing'
-        else:
-            msg = 'camera idle'
-        capturing_status = {'status': self.capturing, 'message': msg}
-
-        return {'status': {'connected': connection_status,
-                           'capturing': capturing_status},
-                'timestamp': int(round(time() * 1000))}
